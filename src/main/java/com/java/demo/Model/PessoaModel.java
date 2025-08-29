18fb9981d2f610d90a4a8579b5ff4385db86cd40
Caio000112 <caiobrunotoledo@gmail.com>
2025-08-28 21:54:06 -0300

    pessoa model

---
+++ src/main/java/com/java/demo/Model/PessoaModel.java
@@ -0,0 +1,58 @@
+package com.java.demo.Model;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Table(name = "Pessoa")
+public class PessoaModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String nome;
+
+    private String cpf;
+
+    private int idade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public PessoaModel(Integer id) {
+        this.id = id;
+    }
+
+    public PessoaModel() {
+    }
+}
